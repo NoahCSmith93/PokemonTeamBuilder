8fb188548c32a08901a877e3939680939106c10f
Noah Smith <noahcsmith1993@gmail.com>
2023-08-28 12:16:52 -0400

    Initial commit

---
+++ config/middleware.js
@@ -0,0 +1,33 @@
+const express = require("express")
+require("dotenv").config()
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require("express-session")
+const passport = require("passport")
+const methodOverride = require("method-override");
+require("./passport")
+
+function middleware(app) {
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
+    app.use(methodOverride("_method"))
+
+    // This sets up our session so passport can use it
+    app.use(session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: true
+    }))
+    app.use(passport.initialize())
+    app.use(passport.session())
+
+    app.use((req, res, next) => {
+        res.locals.user = req.user
+        next()
+    })
+}
+
+module.exports = middleware
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const middleware = require('./config/middleware');
+require('dotenv').config();
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+middleware(app)
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
